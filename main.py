888a58bd55a9231d0374260a163245df9179addb
Devin Sidhu <devinsidhu24@gmail.com>
2023-09-29 17:56:22 +0100

    Created functionality to get JSON object for a YT channel based on provided channel ID, also started working on the jupyter notebook which will parse and visualize the data

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-chanName = input("ENTER CHANNEL NAME: ")
+chanID = input("ENTER CHANNEL ID: ")
+channel = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id={chanID}&key={API_KEY}").json() # Get channel JSON object
 
+# Adding channel data to file
+with open("data.json", "r+") as file:
+    
+    data = json.load(file)
+    data["channels"].append(channel)
+
+    # Delete all existing data in file before writing updated JSON object
+    file.seek(0)
+    file.truncate()
+
+    json.dump(data, file, indent=4)
+
+
+print("Channel has been added")
