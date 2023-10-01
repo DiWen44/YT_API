b82cd62f9874526d0d7ebd2834f50ee7094d531b
Devin Sidhu <devinsidhu24@gmail.com>
2023-10-01 11:19:57 +0100

    Added code to clean away unnecessary JSON data (the youtube#channelListResponse object that wraps the actual pertinent channel data, found in items[0] of that object), as well as error checking for an invalid channel ID

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 import requests
 import json
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-chanID = input("ENTER CHANNEL ID: ")
-channel = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id={chanID}&key={API_KEY}").json() # Get channel JSON object
+chanID = sys.argv[1] # Channel ID is passed as a command-line parameter
+response = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id={chanID}&key={API_KEY}").json() # Get JSON response for channel ID
+
+if response['pageInfo']['totalResults'] == 0:
+    sys.exit('ERROR: No channel found with provided ID')
+
+channel = response["items"][0] # Actual channel data is found in "items" array
 
 # Adding channel data to file
-with open("data.json", "r+") as file:
+with open("data_test.json", "r+") as file:
     
     data = json.load(file)
     data["channels"].append(channel)
