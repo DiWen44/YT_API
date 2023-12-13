5697aa6c85036347df0dfd0189b6a021f1e3b97e
Devin Sidhu <devinsidhu24@gmail.com>
2023-12-13 12:08:55 +0000

    Refactored code in add_channel.py and finished code for extracting data in visual.ipynb

--- add_channel.py
+++ add_channel.py
@@ -4,6 +4,51 @@ import json
 import sys
 from dotenv import load_dotenv
 
+
+"""
+From a provided channel dict, returns an array of dicts representing that channel's videos
+Returned video dicts have fields:
+    name - name of video
+    id - video ID
+    uploadDateTime - date & time of video publication. Stored in a string (In ISO 8601 format) in the JSON file and in the JSON response from the YouTube API, so should be converted to datetime when used.
+    duration - Video's duration. Stored in a string (In ISO 8601 format) in the JSON file and in the JSON response from the YouTube API, so should be converted to datetime when used.
+    views - No. of views
+    likes - No. of likes
+    commentCount - No. of comments
+"""
+def getVideos(channel):
+
+    # A "playlist" object contains "playlistItem" objects, each of which is associated with a video in that playlist.
+    # The "uploads" playlist contains playlistItems for all videos posted by the channel
+    uploadsPlaylistID = chanData['contentDetails']['relatedPlaylists']['uploads']
+    uploadsPlaylistItems = requests.get(f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&playlistId={uploadsPlaylistID}&key={API_KEY}").json()['items'] # Get all playlistItems in the upload playlist.
+
+    # Get IDs of all uploaded videos from their corresponding playlistItems.
+    videoIDs = []
+    for item in uploadsPlaylistItems: 
+        videoIDs.append(item['snippet']['resourceId']['videoId'])
+
+    videoIDs = ','.join(videoIDs) # Convert array into comma-seperated string (no spaces) so it can be passed as a URL parameter
+    vids = requests.get(f"https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={videoIDs}&key={API_KEY}").json()['items'] # Get all videos corresponding to the videoIDs
+
+    # Cleaning video data to get only relevant details
+    vidsCleaned = []
+    for vid in vids:
+        vidCleaned = {
+            'name': vid['snippet']['title'],
+            'id': vid['id'],
+            'uploadDateTime': vid['snippet']['publishedAt'],
+            'duration': vid['contentDetails']['duration'],
+            
+            'views': int(vid['statistics']['viewCount']),
+            'likes': int(vid['statistics']['likeCount']),
+            'commentCount': int(vid['statistics']['commentCount']),
+        }
+        vidsCleaned.append(vidCleaned)
+
+    return vidsCleaned
+
+
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
@@ -26,41 +71,15 @@ response = requests.get(f"https://www.googleapis.com/youtube/v3/channels?part=sn
 if response['pageInfo']['totalResults'] == 0:
     sys.exit('ERROR: No channel found with provided ID')
 
-chanData = response["items"][0] # All actual & pertinent channel data is found in "items" array
-
-# Get channel's videos via the uploads playlist assocated with the channel, which contains all of the videos uploaded by the channel
-uploadsPlaylistID = chanData['contentDetails']['relatedPlaylists']['uploads']
-uploadsPlaylistItems = requests.get(f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&playlistId={uploadsPlaylistID}&key={API_KEY}").json()['items'] # Get all playlistItems in the upload playlist.
-
-# Get IDs of all uploaded videos from their corresponding playlistItems.
-videoIDs = []
-for item in uploadsPlaylistItems: 
-    videoIDs.append(item['snippet']['resourceId']['videoId'])
-
-videoIDs = ','.join(videoIDs) # Convert array into comma-seperated string (no spaces) so it can be passed as a URL parameter
-vids = requests.get(f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id={videoIDs}&key={API_KEY}").json()['items'] # Get all videos corresponding to the videoIDs
-
-# Cleaning video data to get only relevant details
-vidsCleaned = []
-for vid in vids:
-    vidCleaned = {
-        'name': vid['snippet']['title'],
-        'id': vid['id'],
-        'uploadDate': vid['snippet']['publishedAt'],
-
-        'views': vid['statistics']['viewCount'],
-        'likes': vid['statistics']['likeCount'],
-        'commentCount': vid['statistics']['commentCount'],
-    }
-    vidsCleaned.append(vidCleaned)
+chanData = response["items"][0] # response is actually a search response, and since we searched by ID, the first and only search result is desired.
 
 
 # Cleaned & Filtered channel data to be inserted into JSON file
 channel = {
     'name': chanData['snippet']['title'],
     'id': chanData['id'],
-    'subCount': chanData['statistics']['subscriberCount'],  # YouTube rounds this value to 3 significant figures
-    'videos': vidsCleaned
+    'subCount': int(chanData['statistics']['subscriberCount']),  # YouTube rounds this value to 3 significant figures
+    'videos': getVideos(chanData)
 }
 
 
